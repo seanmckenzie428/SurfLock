313ad0dad36ee2802587879e638fae142b67894e
Sean McKenzie <70048908+seanmckenzie428@users.noreply.github.com>
2023-11-16 12:10:09 -0700

    Initial Commit

---
+++ SurfLock/SurfLockApp.swift
@@ -0,0 +1,17 @@
+//
+//  SurfLockApp.swift
+//  SurfLock
+//
+//  Created by Sean McKenzie on 11/16/23.
+//
+
+import SwiftUI
+
+@main
+struct SurfLockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SurfLockUITests/SurfLockUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SurfLockUITestsLaunchTests.swift
+//  SurfLockUITests
+//
+//  Created by Sean McKenzie on 11/16/23.
+//
+
+import XCTest
+
+final class SurfLockUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
