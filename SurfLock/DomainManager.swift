f2a3c2a2d5826699139ff542d2673920aed38cbf
Sean McKenzie <70048908+seanmckenzie428@users.noreply.github.com>
2023-11-16 17:41:00 -0700

    Clean up DomainManager

--- SurfLock/DomainManager.swift
+++ SurfLock/DomainManager.swift
@@ -13,43 +13,39 @@ class DomainManager: ObservableObject {
     @Published var settingsStore = ManagedSettingsStore.init();
     @Published var domains: Set<WebDomain> = []
     
-    var blockedDomains: Set<WebDomain> {
-            print("got blocked domains")
-            return getDomainSetFromFilter(filter: settingsStore.webContent.blockedByFilter)
-        }
+    func getDomainSetFromFilter(filter: WebContentSettings.FilterPolicy?) -> Set<WebDomain> {
+        var domainSetFromAuto: Set<WebDomain> = []
         
-        func getDomainSetFromFilter(filter: WebContentSettings.FilterPolicy?) -> Set<WebDomain> {
-            var domainSetFromAuto: Set<WebDomain> = []
-            
-            if case let .auto(domains, _) = filter {
-                for domain in domains {
-                    if let domainValue = domain.domain {
-                        domainSetFromAuto.insert(WebDomain(domain: domainValue))
-                    }
+        if case let .auto(domains, _) = filter {
+            for domain in domains {
+                if let domainValue = domain.domain {
+                    domainSetFromAuto.insert(WebDomain(domain: domainValue))
                 }
             }
-            
-            return domainSetFromAuto
         }
+        
+        return domainSetFromAuto
+    }
     
-        func syncDomains() {
-            domains = getDomainSetFromFilter(filter: settingsStore.webContent.blockedByFilter)
+    func syncDomains() {
+        domains = getDomainSetFromFilter(filter: settingsStore.webContent.blockedByFilter)
+    }
+    
+    func setBlockedDomains(domainsToBlock: Set<WebDomain>) {
+        settingsStore.webContent.blockedByFilter = .auto(domainsToBlock)
+        syncDomains()
+    }
+    
+    func blockDomain(domain: WebDomain) {
+        guard domain.domain != nil else {
+            return
         }
-        
-        func setBlockedDomains(domainsToBlock: Set<WebDomain>) {
-            settingsStore.webContent.blockedByFilter = .auto(domainsToBlock)
-            syncDomains()
-        }
-        
-        func blockDomain(domain: WebDomain) {
-            var blockedDomains = self.blockedDomains
-            blockedDomains.insert(domain)
-            setBlockedDomains(domainsToBlock: blockedDomains)
-        }
-        
-        func unblockDomain(domain: WebDomain) {
-            var blockedDomains = self.blockedDomains
-            blockedDomains.remove(domain)
-            setBlockedDomains(domainsToBlock: blockedDomains)
-        }
+        domains.insert(domain)
+        setBlockedDomains(domainsToBlock: domains)
+    }
+    
+    func unblockDomain(domain: WebDomain) {
+        domains.remove(domain)
+        setBlockedDomains(domainsToBlock: domains)
+    }
 }
